6a822457f608a89f08d4e8155c72c01f58fd97d6
vadlamb <vadlambrianov@gmail.com>
2024-05-19 14:55:58 +0300

    Fix auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,9 @@ export const metadata: Metadata = {
     default: 'Acme Dashboard',
   },
   description: 'The official Next.js course',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  metadataBase: new URL(
+    'https://next-tutorial-3gyp5e0gy-vadims-projects-2230f5f0.vercel.app/',
+  ),
 };
 
 export default function RootLayout({
