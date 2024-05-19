50513703439fa96268a956e9627061a7cff841f9
vadlamb <vadlambrianov@gmail.com>
2024-05-19 15:01:43 +0300

    Fixed error on invoice delete

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -120,7 +120,6 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
   try {
     await sql`
   DELETE FROM invoices WHERE id = ${id}
